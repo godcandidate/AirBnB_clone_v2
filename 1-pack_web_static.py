fa1ea22436143ceb6505c383cc56f099a02f34ab
godcandidate <godcandidate@gmail.com>
2023-05-15 13:02:23 +0000

    update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 from fabric.api import local
-from datetime import datetime
+from time import strftime
+from datetime import date
 
 
 def do_pack():
     """ A script that generates archive the contents of web_static folder"""
 
-    dt = datetime.now()
-    date_t = f"{dt.year}{dt.month}{dt.day}{dt.hour}{dt.minute}{dt.second}"
-    filename = f"versions/web_static_{date_t}.tgz"
-
+    file = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -czvf {}.tgz".format(filename))
-        return "{}.tgz".format(filename)
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(file))
+
+        return "versions/web_static_{}.tgz".format(file)
 
     except Exception as e:
         return None
