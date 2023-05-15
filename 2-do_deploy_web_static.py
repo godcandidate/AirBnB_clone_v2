29a1e8391e26ac12cdc7d202cf4e43f437fa7aa9
godcandidate <godcandidate@gmail.com>
2023-05-15 18:49:47 +0000

    update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Deploy an archive of static html to my web servers with Fabric3"""
 
-from fabric.api import *
+from fabric import api
 from fabric.contrib import files
 import os
 
 
-env.hosts = ['3.84.239.114', '34.224.17.58']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/alx_server'
+api.env.hosts = ['3.84.239.114', '34.224.17.58']
+api.env.user = 'ubuntu'
+api.env.key_filename = '~/.ssh/school'
 
 
-def deploy_archive(archive_path):
+def do_deploy(archive_path):
     """Function to transfer `archive_path` to web servers.
     Args:
         archive_path (str): path of the .tgz file to transfer
@@ -19,23 +19,23 @@ def deploy_archive(archive_path):
     """
     if not os.path.isfile(archive_path):
         return False
-    with cd('/tmp'):
-        file_name = os.path.basename(archive_path)
-        new_path, ext = os.path.splitext(file_name)
+    with api.cd('/tmp'):
+        basename = os.path.basename(archive_path)
+        root, ext = os.path.splitext(basename)
+        outpath = '/data/web_static/releases/{}'.format(root)
         try:
-            put_path = put(archive_path)
-            if files.exists(new_path):
-                run('rm -rdf {}'.format(new_path))
-            run('mkdir -p {}'.format(new_path))
-            run('tar -xzf {} -C {}'.format(put_path[0], new_path))
-            run('rm -f {}'.format(put_path[0]))
-            run('mv -u {}/web_static/* {}'.format(new_path, new_path))
-            run('rm -rf {}/web_static'.format(new_path))
-            run('rm -rf /data/web_static/current')
-            run('ln -sf {} /data/web_static/current'.format(new_path))
+            putpath = api.put(archive_path)
+            if files.exists(outpath):
+                api.run('rm -rdf {}'.format(outpath))
+            api.run('mkdir -p {}'.format(outpath))
+            api.run('tar -xzf {} -C {}'.format(putpath[0], outpath))
+            api.run('rm -f {}'.format(putpath[0]))
+            api.run('mv -u {}/web_static/* {}'.format(outpath, outpath))
+            api.run('rm -rf {}/web_static'.format(outpath))
+            api.run('rm -rf /data/web_static/current')
+            api.run('ln -sf {} /data/web_static/current'.format(outpath))
             print('New version deployed!')
         except:
             return False
         else:
             return True
-
