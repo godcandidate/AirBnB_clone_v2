473c7aa0373e020cd2bd8cf1b6652f5509987514
godcandidate <godcandidate@gmail.com>
2023-05-25 21:35:20 +0000

    update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Module - script that starts a Flask web application"""
+"""a script that starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Handles the root url"""
+    """This handles the root url"""
     return 'Hello HBNB!'
 
 
