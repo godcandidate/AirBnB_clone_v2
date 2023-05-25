fef9523df1e0458fd761617130bfb16ae6dda97d
godcandidate <godcandidate@gmail.com>
2023-05-25 22:27:12 +0000

    update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
-"""Module - script that starts a Flask web application"""
+"""this odule - script that starts a Flask web application"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Handles the root url"""
+    """this handles the root url"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Handles hbnb route"""
+    """this handles hbnb route"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """Handles /c/<text> route"""
+    """this handles /c/<text> route"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
-    """Handles /python/<text> route"""
+    """this handles /python/<text> route"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """Handles /number/<n> route"""
+    """this handles /number/<n> route"""
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Handles /number_template/<int:n> route"""
+    """this handles /number_template/<int:n> route"""
     return render_template('5-number.html', n=n)
 
 
